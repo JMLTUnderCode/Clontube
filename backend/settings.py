0c96baea150c46a96598eacb38182d22b5ad0dc5
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-17 14:28:18 -0400

    Actualizacion de port bajo env

--- backend/settings.py
+++ backend/settings.py
@@ -84,7 +84,7 @@ if os.environ.get('RENDER', None):
             'USER': os.environ.get('DB_USER'),
             'PASSWORD': os.environ.get('DB_PASSWORD'),
             'HOST': os.environ.get('DB_HOST'),
-            'PORT': '5432',
+            'PORT': os.environ.get('DB_PORT'),
         }
     }
 else:

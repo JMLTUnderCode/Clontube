2d1259b25673139e87c4b26debafb6bd05cf7806
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-18 22:30:12 -0400

    Ocultar la interfaz de exploracion de Django Rest Framework

--- backend/settings.py
+++ backend/settings.py
@@ -59,6 +59,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),

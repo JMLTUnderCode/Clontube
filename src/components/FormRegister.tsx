2336a6aab245b69804ab0dac8a4c8caedd229752
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-21 00:22:05 -0400

    Se implementa mensaje de error en caso de campos como email o contraseña no coincidan.

--- src/components/FormRegister.tsx
+++ src/components/FormRegister.tsx
@@ -12,21 +12,91 @@ export function FormRegister() {
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
 
+    const [emailError, setEmailError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
     useEffect(() => {
         LOGIN_VIEW_STATE.registerFields.full_name = full_name;
         LOGIN_VIEW_STATE.registerFields.username = username;
         LOGIN_VIEW_STATE.registerFields.email = email;
     }, [LOGIN_VIEW_STATE.registerFields, full_name, username, email]);
 
+    function handleEmailBlur() {
+        if (email && confirmEmail && email !== confirmEmail) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+    }
+
+    function handleConfirmEmailBlur() {
+        if (email && confirmEmail && email !== confirmEmail) {
+            setEmailError(true);
+        } else {
+            setEmailError(false);
+        }
+    }
+
+    function handlePasswordBlur() {
+        if (password && confirmPassword && password !== confirmPassword) {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+        }
+    }
+
+    function handleConfirmPasswordBlur() {
+        if (password && confirmPassword && password !== confirmPassword) {
+            setPasswordError(true);
+        } else {
+            setPasswordError(false);
+        }
+    }
+
     return (
         <form className="register-form">
             <h2>Registrarse</h2>
             <Input type="FULL_NAME" label="Nombre Completo" field={full_name} onChange={setFullName} />
             <Input type="USERNAME" label="Usuario" field={username} onChange={setUsername} />
-            <Input type="EMAIL" label="Correo Electrónico" field={email} onChange={setEmail} />
-            <Input type="EMAIL" label="Confirmar Correo Electrónico" field={confirmEmail} onChange={setConfirmEmail} />
-            <Input type="PASSWORD" label="Contraseña" field={password} onChange={setPassword} />
-            <Input type="PASSWORD" label="Confirmar Contraseña" field={confirmPassword} onChange={setConfirmPassword} />
+            <Input
+                type="EMAIL"
+                label="Correo Electrónico"
+                field={email}
+                onChange={setEmail}
+                onBlur={handleEmailBlur}
+                error={emailError}
+            />
+            {emailError && (
+                <div className="input-error-popup">Los correos electrónicos no coinciden.</div>
+            )}
+            <Input
+                type="EMAIL"
+                label="Confirmar Correo Electrónico"
+                field={confirmEmail}
+                onChange={setConfirmEmail}
+                onBlur={handleConfirmEmailBlur}
+                error={emailError}
+            />
+            
+            <Input
+                type="PASSWORD"
+                label="Contraseña"
+                field={password}
+                onChange={setPassword}
+                onBlur={handlePasswordBlur}
+                error={passwordError}
+            />
+            {passwordError && (
+                <div className="input-error-popup">Las contraseñas no coinciden.</div>
+            )}
+            <Input
+                type="PASSWORD"
+                label="Confirmar Contraseña"
+                field={confirmPassword}
+                onChange={setConfirmPassword}
+                onBlur={handleConfirmPasswordBlur}
+                error={passwordError}
+            />
             <button className="btn" type="submit">Registro</button>
         </form>
     );
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -7,10 +7,12 @@ type InputProps = {
     type: Promps;
     label: string;
     field: string;
+    error?: boolean;
+    onBlur?: () => void;
     onChange: (value: string) => void;
 }
 
-export function Input({ type, label, field, onChange } : InputProps) {
+export function Input({ type, label, field, error, onChange, onBlur } : InputProps) {
     const [showPassword, setShowPassword] = useState(false);
     
     const handleTogglePassword = () => {
@@ -22,10 +24,12 @@ export function Input({ type, label, field, onChange } : InputProps) {
     return (
         <div className="input-group">
             <input
+                className={error ? 'input-error' : ''}
                 type={typeInput}
                 id={useId()}
                 value={field}
                 onChange={e => onChange(e.target.value)}
+                onBlur={onBlur}
                 required
                 placeholder=" "
             />

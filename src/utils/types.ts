109a669dd4cdf0ac6278d6b184f6dc43f41c0229
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-24 10:46:23 -0400

    Modularizacion de tipos para mejor legibilidad en la declaracion de tipos

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,25 +1,33 @@
 export type ViewType = 'login' | 'register';
 
+export type Register = {
+    full_name: string;
+    username: string;
+    email: string;
+};
+
+export type Login = {
+    identifier: string;
+};
+
 export type LoginViewState = {
     view: ViewType;
-    loginFields: {
-        identifier: string;
-    };
-    registerFields: {
-        full_name: string;
-        username: string;
-        email: string;
-    };
+    loginFields: Login;
+    registerFields: Register;
 };
 
 export type LoginViewContextType = {
     LOGIN_VIEW_STATE: LoginViewState;
     showLogin: () => void;
+    setLoginFields: (loginFields: Login) => void;
     showRegister: () => void;
+    setRegisterFields: (registerFields: Register) => void;
     clearFields: () => void;
 };
 
 export type Action = 
-    | { type: 'SET_LOGIN_VIEW'; payload: Partial<LoginViewState['registerFields']> }
-    | { type: 'SET_REGISTER_VIEW'; payload: Partial<LoginViewState['loginFields']> }
+    | { type: 'SET_LOGIN_VIEW'; payload: Register }
+    | { type: 'SET_LOGIN_FIELDS'; payload: Login }
+    | { type: 'SET_REGISTER_VIEW'; payload: Login }
+    | { type: 'SET_REGISTER_FIELDS'; payload: Register }
     | { type: 'RESET_FIELDS' };

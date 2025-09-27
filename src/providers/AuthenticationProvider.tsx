7d28e9d05a4237bd670f9114c53d40f26b861acb
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-27 11:34:16 -0400

    Implementacion de reducer y provider para authenticacion de usuarios. Se cuenta con 3 acciones clave, login que guardan el token JWT de acceso y refresco junto a la informacion del usuario. Logout resetea el estado. Login failed actualiza los intentos fallidos de login.

---
+++ src/providers/AuthenticationProvider.tsx
@@ -0,0 +1,55 @@
+import React, { useCallback, useEffect, useReducer } from 'react';
+import { authenticationReducer, AuthState } from '../reducers/authenticationReducer';
+import { AuthenticationContext } from '../contexts/AuthenticationContext';
+import type { AuthStateType } from '../utils/types';
+
+function updateLocalStorageAuth(state: AuthStateType) {
+    localStorage.setItem('authState', JSON.stringify(state));
+};
+
+function useAuthReducer() {
+    const [state, dispatch] = useReducer(authenticationReducer, AuthState);
+
+    const login = useCallback(async (identifier: string, password: string) => {
+        try {
+            //const res = await fetch('/api/login/', {
+            const res = await fetch('https://backend-9tcm.onrender.com/api/login/', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ identifier, password }),
+            });
+            const data = await res.json();
+            if (res.ok && data.success) {
+                dispatch({ type: 'LOGIN_SUCCESS', payload: { user: data.user, access: data.access, refresh: data.refresh } });
+                return true;
+            } else {
+                dispatch({ type: 'LOGIN_FAILURE' });
+                return false;
+            }
+        } catch {
+            dispatch({ type: 'LOGIN_FAILURE' });
+            return false;
+        }
+    }, []);
+
+    const logout = useCallback(() => {
+        dispatch({ type: 'LOGOUT' });
+    }, []);
+
+    return { state, login, logout };
+}
+
+export function AuthenticationProvider({ children }: { children: React.ReactNode }) {
+    const { state, login, logout } = useAuthReducer();
+
+    // Sincroniza localStorage cada vez que el estado cambia
+    useEffect(() => {
+        updateLocalStorageAuth(state);
+    }, [state]);
+
+    return (
+        <AuthenticationContext.Provider value={{ AUTH_STATE: state, login, logout }}>
+            {children}
+        </AuthenticationContext.Provider>
+    );
+}

e6893f62f9852e584706769ea8ffeb23b34a2574
JMLTUnderCode <juniormiguel08062@gmail.com>
2025-09-27 11:30:24 -0400

    Se implementa React Router para navegacion entre componentes. Se protege la ruta main en caso de no haber login activo. Se incluye la pagina principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import './styles/App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import { StartPage } from './pages/StartPage';
+import { MainPage } from './pages/MainPage';
 import { Footer } from './components/Footer';
 
 export function App() {
-    return (
-        <div className="App">
-            <StartPage />
-            <Footer />
-        </div>
+     return (
+        <BrowserRouter basename="/Clontube/">
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={<StartPage />} />
+                    <Route path="/main/" element={ 
+                        <ProtectedRoute>
+                            <MainPage />
+                        </ProtectedRoute>
+                    } />
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
     );
 };
